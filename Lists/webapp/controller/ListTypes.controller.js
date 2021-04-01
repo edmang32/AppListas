ebf174acb51cb28750ecfcdd8b5c9e05ebc9f95f
edmang32 <edmang32@gmail.com>
2021-04-01 19:30:42 +0000

    Firts commit to 01 App Listas

---
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -0,0 +1,90 @@
+// @ts-nocheck
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ns.Lists.controller.ListTypes", {
+			onInit: function () {
+                var oJSONModel = new sap.ui.model.json.JSONModel();
+                oJSONModel.loadData("./localService/mockdata/ListData.json");
+                this.getView().setModel(oJSONModel);
+            },
+        
+            getGroupHeader: function(oGroup) {
+                var groupHeaderListItem = new sap.m.GroupHeaderListItem({
+                    title: oGroup.key,
+                    upperCase: true
+                });
+                return groupHeaderListItem;
+            },
+
+            onShowSelectedRows: function () {
+                var stadardList = this.getView().byId("stadardList");
+                var selectedItems = stadardList.getSelectedItems();
+
+                var i18nModel= this.getView().getModel("i18n").getResourceBundle();
+                if (selectedItems.length === 0){
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"));
+                }else{
+                    var textMessage = i18nModel.getText("selection")
+                    for(var item in selectedItems){
+                        var context  = selectedItems[item].getBindingContext();
+                        var oContext = context.getObject();
+                        textMessage = textMessage + " - " + oContext.Material; 
+                    }
+                    sap.m.MessageToast.show(textMessage);
+                }
+            },
+
+            onDeleteRows: function () {
+                var stadardList = this.getView().byId("stadardList");
+                var selectedItems = stadardList.getSelectedItems();
+                var i18nModel= this.getView().getModel("i18n").getResourceBundle();
+                if (selectedItems.length === 0){
+                    sap.m.MessageToast.show(i18nModel.getText("noSelection"));
+                }else{
+                    var textMessage = i18nModel.getText("selection");
+                    var model = this.getView().getModel();
+                    var products = model.getProperty("/Products");
+                    
+                    var arrayId = [];
+                    for (var i in selectedItems){
+                        var context  = selectedItems[i].getBindingContext();
+                        var oContext = context.getObject();
+
+                        arrayId.push(oContext.Id)
+                        textMessage = textMessage + " - " + oContext.Material;
+                    }
+                    products = products.filter(function (p) {
+                       return !arrayId.includes(p.Id); 
+                    });
+
+                    model.setProperty("/Products", products);
+                    stadardList.removeSelections();
+                    sap.m.MessageToast.show(textMessage);
+                }
+            },
+
+            onDeletethisRow: function (oEvent) {
+                var i18nModel= this.getView().getModel("i18n").getResourceBundle();
+                var textMessage = i18nModel.getText("selectedRow");
+                var selectedRow = oEvent.getParameter("listItem");
+                var context = selectedRow.getBindingContext();
+                var splithPath = context.getPath().split("/");
+                var indexSelectedRow = splithPath[splithPath.length - 1];
+                var model = this.getView().getModel();
+                var products = model.getProperty("/Products");
+                products.splice(indexSelectedRow, 1);
+                let actualRow = indexSelectedRow;
+                ++actualRow;
+                textMessage = textMessage + ':' + actualRow;
+                sap.m.MessageToast.show(textMessage);
+                model.refresh();
+            }
+		});
+	});
